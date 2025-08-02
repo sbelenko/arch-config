3af87197734ef3c24cbfed0e1f31200d965cc305
Serhii <>
2025-08-02 12:28:54 +0300

    up

---
+++ arch_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# List of packages
+packages=(
+    nano # Console text editor
+    kitty # Terminal
+    hyprland # Tiling Wayland compositor
+    hyprpaper # Wallpaper utility
+    hyprlock # Screen locker
+    xdg-desktop-portal-hyprland # XDG portal for Wayland apps
+    sddm # Display manager
+    flatpak # Package manager for sandboxed apps
+    waybar # Wayland status bar
+    nautilus # File manager
+    wofi # Application launcher
+    mako # Notification daemon
+    git # Version control system
+)
+
+# Update the system
+sudo pacman -Syu --noconfirm
+
+# Install the packages
+sudo pacman -S "${packages[@]}" --noconfirm
+
+# System setup
+sudo systemctl enable sddm # Enable sddm
+
