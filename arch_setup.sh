f915ccbad95acd3e4964686cce938fdaa3563372
Serhii <>
2025-08-02 12:39:02 +0300

    up

--- arch_setup.sh
+++ arch_setup.sh
@@ -2,19 +2,19 @@
 
 # List of packages
 packages=(
-    nano # Console text editor
-    kitty # Terminal
-    hyprland # Tiling Wayland compositor
-    hyprpaper # Wallpaper utility
-    hyprlock # Screen locker
-    xdg-desktop-portal-hyprland # XDG portal for Wayland apps
-    sddm # Display manager
-    flatpak # Package manager for sandboxed apps
-    waybar # Wayland status bar
-    nautilus # File manager
-    wofi # Application launcher
-    mako # Notification daemon
-    git # Version control system
+  nano # Console text editor
+  kitty # Terminal
+  hyprland # Tiling Wayland compositor
+  hyprpaper # Wallpaper utility
+  hyprlock # Screen locker
+  xdg-desktop-portal-hyprland # XDG portal for Wayland apps
+  sddm # Display manager
+  flatpak # Package manager for sandboxed apps
+  waybar # Wayland status bar
+  nautilus # File manager
+  wofi # Application launcher
+  mako # Notification daemon
+  git # Version control system
 )
 
 # Update the system
