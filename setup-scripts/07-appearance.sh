c5e40fcf7384bb33bbda3d330ae008a5119e16ac
Serhii <belenkosergey@gmail.com>
2026-01-18 23:42:20 +0200

    up

--- setup-scripts/07-appearance.sh
+++ setup-scripts/07-appearance.sh
@@ -1,12 +1,11 @@
 echo "-> Installing and applying icon theme..."
 
 USER_ICONS_DIR="$HOME/.icons"
-ICON_THEME_ARCHIVE="$SCRIPT_DIR/themes/icons/Reversal-orange.tar.xz"
 ICON_THEME_NAME="Reversal-orange"
 
 # Create directory and unpack theme
 mkdir -p "$USER_ICONS_DIR" || handle_warning "Failed to create icon directory"
-tar -xf "$ICON_THEME_ARCHIVE" -C "$USER_ICONS_DIR" || handle_warning "Failed to unpack icon theme"
+tar -xf "$SCRIPT_DIR/themes/icons/Reversal-orange.tar.xz" -C "$USER_ICONS_DIR" || handle_warning "Failed to unpack icon theme"
 
 # Set the icon theme
 gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME_NAME" || handle_warning "Failed to set icon theme (is gsettings installed?)"
