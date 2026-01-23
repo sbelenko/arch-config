928995e758a3bc983cea60fef4c8ebbb0a19a1a1
Serhii <belenkosergey@gmail.com>
2026-01-23 20:09:27 +0200

    up

--- setup-scripts/07-appearance.sh
+++ setup-scripts/07-appearance.sh
@@ -1,11 +1,16 @@
+# Icons
 echo "-> Installing and applying icon theme..."
-
 USER_ICONS_DIR="$HOME/.icons"
-ICON_THEME_NAME="Reversal-orange"
 
 # Create directory and unpack theme
 mkdir -p "$USER_ICONS_DIR" || handle_warning "Failed to create icon directory"
 tar -xf "$SCRIPT_DIR/themes/icons/Reversal-orange.tar.xz" -C "$USER_ICONS_DIR" || handle_warning "Failed to unpack icon theme"
 
 # Set the icon theme
-gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME_NAME" || handle_warning "Failed to set icon theme (is gsettings installed?)"
+gsettings set org.gnome.desktop.interface icon-theme "Reversal-orange" || handle_warning "Failed to set icon theme (is gsettings installed?)"
+
+# Cursor
+echo "-> Installing and applying cursor theme..."
+ICONS_DIR="/usr/share/icons"
+mkdir -p "$ICONS_DIR" || handle_warning "Failed to create icon directory"
+tar -xf "$SCRIPT_DIR/themes/icons/Future-cursors.tar.gz" -C "$ICONS_DIR" || handle_warning "Failed to unpack icon theme"
